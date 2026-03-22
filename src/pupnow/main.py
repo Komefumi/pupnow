36d527e7b79f610ae78631ac5beaf3d28a1aa064
Kome Fumi <komefumi@protonmail.ch>
2026-03-22 13:56:49 +0000

    1. Update test logic with fixture 2. Make catalogue endpoint

--- src/pupnow/main.py
+++ src/pupnow/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
   return {"Hello": "World"}
+
+@app.get("/catalogue")
+def open_catalogue():
+  return [{"item_name": "aaa"}]
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,9 @@
 from fastapi.testclient import TestClient
 from pupnow.main import app
 
-client = TestClient(app)
 
-def test_read_main():
-  # 1. Arrange & Act
-  response = client.get("/")
+def test_read_main(get):
+  assert get("/") == {"Hello": "World"}
 
-  # 2. Assert
-  assert response.status_code == 200
-  assert response.json() == {"Hello": "World"}
+def test_get_catalogue(get):
+  isinstance(get("/catalogue"), list)
